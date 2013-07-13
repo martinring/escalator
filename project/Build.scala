3c5fcce887a76a4039810f16b184aa8e64b4ec7f
Fynn Feldpausch <f.feldpausch@live.de>
2013-07-13 17:25:08 +0200

    started layouting
    using webjars
    removed old css/js

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,15 @@ import play.Project._
 object ApplicationBuild extends Build {
   val appName         = "escalator"
   val appVersion      = "1.0-SNAPSHOT"
-
+  
   val appDependencies = Seq(
-    "org.scala-lang" % "scala-compiler" % "2.10.0"
+    "org.scala-lang" % "scala-compiler" % "2.10.0",
+    "org.webjars" %% "webjars-play" % "2.1.0-2",
+    "org.webjars" % "jquery" % "2.0.3",
+    "org.webjars" % "codemirror" % "3.14"
   )
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(    
+  
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    
   )
 }
